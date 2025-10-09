1866bc7e4b44a2f74b48e44d37c5536c42d3caf5
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-09 11:22:05 +0530

    malloc.c
    malloc() – Allocates a single block of memory.
    
    int *p = (int*) malloc(5 * sizeof(int)); // prototype

---
+++ Dynamic Memory Allocation/malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *p=NULL;
+    p=(int *)malloc(2*sizeof(int));
+    int i,j;
+    printf("enter n elements\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&p[i]);
+    }
+    printf("overall output elements\n");
+    for(j=0;j<n;j++){
+        printf("%d ",p[j]);
+    }
+}
