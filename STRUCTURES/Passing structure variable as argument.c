2f795ab5dcb70ecbe0f042fe3bdce06b26a4c584
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-20 11:23:14 +0530

    Update Passing structure variable as argument.c

--- STRUCTURES/Passing structure variable as argument.c
+++ STRUCTURES/Passing structure variable as argument.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+struct student{
+    int i;
+    char c;
+};
+void pf(struct student p){
+    printf("%d %c\n",p.i,p.c);
+}
+int main()
+{
+    struct student cs={1,'A'};
+    struct student cs1={1,'B'};
+    pf(cs);
+    pf(cs1);
+}
