a51611cca0fd09094e53ac06000dd7179ca556a3
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-20 12:21:24 +0530

    Update Passing array of structures as argument.c

--- STRUCTURES/Passing array of structures as argument.c
+++ STRUCTURES/Passing array of structures as argument.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+struct student{
+    int x;
+    int y;
+};
+void pf(struct student *arr){
+    int i;
+    for(i=0;i<3;i++){
+      printf("%d %d\n",arr[i].x,arr[i].y);
+    }
+}
+int main()
+{
+    struct student arr[3]={{1,2},{3,4},{5,6}};
+    pf(arr);
+}
