1e52113424cbc7e85bfe7f36af0c8386ee7e60ed
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-20 11:36:37 +0530

    Update Passing pointers to structures as arguments.c

--- STRUCTURES/Passing pointers to structures as arguments.c
+++ STRUCTURES/Passing pointers to structures as arguments.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+struct student{
+    int i;
+    char c;
+};
+void pf(struct student *ptr){
+    printf("%d %c\n",ptr->i,ptr->c);
+}
+int main()
+{
+    struct student cs1={1,'A'};
+    struct student cs2={1,'B'};
+    pf(&cs1);
+    pf(&cs2);
+}
