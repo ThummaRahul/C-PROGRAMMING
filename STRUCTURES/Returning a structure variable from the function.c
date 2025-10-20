27eb94b2071ee09e9848203434dde5bcb657c028
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-20 11:47:41 +0530

    Update Returning a structure variable from the function.c

--- STRUCTURES/Returning a structure variable from the function.c
+++ STRUCTURES/Returning a structure variable from the function.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+struct student{
+    int i;
+    char c;
+};
+struct student edit(struct student p){
+    (p.i)++;
+    p.c = p.c + 5;
+    return p;
+}
+void pf(struct student *p){
+    printf("%d %c\n",p->i,p->c);
+}
+int main()
+{
+    struct student p1={1,'A'};
+    struct student p2={1,'B'};
+    p1=edit(p1);
+    p2=edit(p2);
+    pf(&p1);
+    pf(&p2);
+}
