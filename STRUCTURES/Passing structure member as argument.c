7fdb298b1a73f07d33aece65aa33be8864849122
Thumma Rahul <rahulthumma87@gmail.com>
2025-10-20 11:26:44 +0530

    Update Passing structure member as argument.c

--- STRUCTURES/Passing structure member as argument.c
+++ STRUCTURES/Passing structure member as argument.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+struct student{
+    int i;
+    char c;
+};
+void keyvalue(int* i,char* c){
+    scanf("%d %c",i,c);
+}
+int main()
+{
+    struct student cs;
+    keyvalue(&cs.i,&cs.c);
+    printf("%d %c",cs.i,cs.c);
+}
